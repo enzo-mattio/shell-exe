6f936ef386f2fb44eded6da717405f981e1fc085
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2022-10-14 10:50:48 +0200

    Réglages problème d'ajout droit super-utilisateurs

--- job9/accessrights.sh
+++ job9/accessrights.sh
@@ -1,18 +1,17 @@
-#./bin/bash 
+#!/bin/bash 
 MY_INPUT='/home/kkli/Bureau/job9/Shell_Userlist.csv'
 while IFS=, read -r columnId columnPrenom columnNom columnMdp columnRole || [ -n "$columnRole" ];
 do 
 	varusername=$columnPrenom$columnNom
 	varpassword=$columnMdp
 	cleanusername="$(echo "${varusername}" | tr -d '[:space:]')"
-	sudo useradd -p $(openssl passwd -1 $varpassword) $cleanusername
-	if [ $columnrole=Admin ];
-then 
-	sudo usermod -aG sudo $cleanusername
-fi
+	sudo useradd -p $(openssl passwd -1 $varpassword) $varusername
+	if [[ $columnRole == A* ]]; then 
+		sudo usermod -aG sudo $cleanusername
+	fi
 echo "User created : $varusername  Password : $varpassword"
 done < $MY_INPUT
 
-sudo userdel PrenomNom
-
 cp Shell_Userlist.csv Shell_Userlist_Backup.csv
+
+sudo userdel PrenomNom
